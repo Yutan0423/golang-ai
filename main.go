8907208573fc6bda4e309fd562416bcb1054102f
Yutan0423 <yuutan3240@gmail.com>
2023-08-20 01:39:09 +0900

    first commit

--- main.go
+++ main.go
@@ -1,22 +1,88 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"fmt"
+	"golang-ai/entity"
+	"golang-ai/util"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 
-	"github.com/tmc/langchaingo/llms/openai"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
+const openaiURL = "https://api.openai.com/v1/chat/completions"
+
+var messages []entity.Message
+
 func main() {
-	llm, err := openai.New()
-	if err != nil {
-		log.Fatal(err)
+	r := mux.NewRouter()
+	r.HandleFunc("/ai", AIHandler).Methods("GET")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func AIHandler(w http.ResponseWriter, r *http.Request) {
+	messages = append(messages, entity.Message{
+		Role:    "user",
+		Content: entity.Prompt,
+	})
+	apiKey := util.GetEnv("OPENAI_API_KEY")
+
+	res := getOpenAIResponse(apiKey)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res.Choices[0].Messages.Content)
+}
+
+func getOpenAIResponse(apiKey string) entity.OpenaiResponse {
+	requestBody := entity.OpenaiRequest{
+		Model:    "gpt-3.5-turbo",
+		Messages: messages,
 	}
 
-	prompt := "What would be a good company name for a company that makes colorful socks?"
-	completion, err := llm.Call(context.Background(), prompt)
+	requestJson, _ := json.Marshal(requestBody)
+
+	req, err := http.NewRequest("POST", openaiURL, bytes.NewBuffer(requestJson))
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(completion)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
+
+	c := &http.Client{}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var res entity.OpenaiResponse
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		println("Error: ", err.Error())
+		return entity.OpenaiResponse{}
+	}
+
+	fmt.Printf("res: %v", res.Choices)
+	messages = append(messages, entity.Message{
+		Role:    "assistant",
+		Content: res.Choices[0].Messages.Content,
+	})
+	return res
 }
